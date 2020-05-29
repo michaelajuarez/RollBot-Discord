cd73f453f3079c897ed9778125ae490cb9d6e911
michaelajuarez <michaelajuarez@gmail.com>
2020-05-28 21:37:33 -0700

    Modifying the DM code.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ client.on("message", msg => {
   var tmp = msg.content.toUpperCase()
   var authorRole = msg.member.roles.highest.name
   var author = msg.member.id
+  var msgAuthor = msg.member
   var rollTaker = "162997214199676928"
   let students = []
   if (tmp === "!ROLL" && (authorRole === "@the_boys" || author === "162997214199676928" || author == "172143655518208000")) {
@@ -31,15 +32,19 @@ client.on("message", msg => {
       collector.on('end', collected => {
         console.log(`Collected "${students.length}" items`)
         botMsg.edit("Alright, that's all for now folks!")
-        
-        // For some reason, sending a DM breaks the bot
-        // const rollTaker = client.users.cache.get(rollTaker)
-        // rollTaker.send('Test')
 
-        // So, for now, I decided to just have it print out in the channel in which it is called. Will need to fix later.
         students.sort()
         var printArr = students.join('\n')
-        msg.channel.send('>>> __***Rollcall:***__\n' + printArr)
+        
+        // For some reason, sending a DM breaks the bot
+        // msgAuthor.send('>>> __***Rollcall:***__\n' + printArr)
+
+        // So, for now, I decided to just have it print out in the channel in which it is called. Will need to fix later.
+        if (students.length === 0) {
+          msg.channel.send('No One is Here!')
+        } else {
+          msg.channel.send('>>> __***Rollcall:***__\n' + printArr)
+        }
       })
     })
   }
